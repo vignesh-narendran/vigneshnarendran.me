2ec0a414c3e1f024ef4668453c6919ba37db0b18
Vignesh Narendran <vignesh-narendran@users.noreply.github.com>
2021-01-02 11:24:07 +0530

    redid animation

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 $(document).ready(() => {
 	gsap.registerPlugin(ScrollTrigger);
 	gsap.to('#slide-1', {
+		duration: 5,
+		xPercent: 100,
+		ease: 'linear',
 		scrollTrigger: {
 			trigger: '#slide-1',
-			scrub: 2,
-			pin: true,
-			start: 'top top',
-			end: 'bottom top',
-		},
-		width: 0,
-		ease: 'none',
-	});
-	gsap.to('#slide-2', {
-		scrollTrigger: {
-			trigger: '#slide-2',
+			markers: {
+				startColor: 'orange',
+				endColor: 'violet',
+				fontSize: '25px',
+				indent: 20,
+			},
 			scrub: 1,
 			pin: true,
+			anticipatePin: 1,
 			start: 'top top',
 			end: 'bottom top',
 		},
-		top: '0',
-		ease: 'none',
+	});
+	gsap.to('#slide-2', {
+		duration: 5,
+		autoAlpha: 1,
+		ease: 'linear',
+		scrollTrigger: {
+			trigger: '#slide-2',
+			markers: {
+				startColor: 'red',
+				endColor: 'green',
+				fontSize: '40px',
+				indent: 40,
+			},
+			scrub: 2,
+			start: 'top bottom',
+			end: 'bottom bottom',
+		},
+	});
+	gsap.to('#slide-2', {
+		duration: 5,
+		xPercent: 100,
+		ease: 'linear',
+		scrollTrigger: {
+			trigger: '#slide-2',
+			markers: {
+				startColor: 'orange',
+				endColor: 'violet',
+				fontSize: '25px',
+				indent: 20,
+			},
+			scrub: 2,
+			pin: true,
+			anticipatePin: 1,
+			start: 'top top',
+			end: 'bottom top',
+		},
 	});
 });
